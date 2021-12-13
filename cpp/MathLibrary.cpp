d4bb8a1e52df688aa871b06c1e89ac20cf94010e
kozheurovdenis <kozheurovdenis@gmail.com>
2021-12-13 16:44:06 +0300

    Добавьте файлы проекта.

---
+++ cpp/MathLibrary.cpp
@@ -0,0 +1,11 @@
+// MathLibrary.cpp : Defines the exported functions for the DLL.
+#include "pch.h" // use stdafx.h in Visual Studio 2017 and earlier
+#include <utility>
+#include <limits.h>
+#include "MathLibrary.h"
+int summ(
+    int a,
+    int b)
+{
+    return a + b;
+}
---
+++ cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace cs
+{
+    class Program
+    {
+        [DllImport(@"C:\Users\User\source\repos\test\x64\Debug\cpp.dll")]
+        static extern int summ( int a, int b);
+        static void Main(string[] args)
+        {
+            Console.WriteLine(summ(5, 2));
+        }
+    }
+}
